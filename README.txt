4e6f57e316e9424894aa9c51c4d2409b5f42de81
mk55 <m55kern@icloud.com>
2014-08-05 17:08:49 +0200

    Kommentare in der Prolog-Datei hunzugefuegt README erstellt

---
+++ README.txt
@@ -0,0 +1,5 @@
+Hinweise zu diesem TestProjekt
+==============================
+
+
+das ist nur ein Test
--- test.pl
+++ test.pl
@@ -1,15 +1,18 @@
+% Testpraedikate fuer die Berechnung der Fakultaet einer nat. Zahl
 fak(0,1).
 fak(N,Fak):- N>0, M is N-1,
              fak(M,F1),
              Fak is N*F1.
 
-
+% 2. Version, hier aber mit vertauschter Reihenfole der Klauseln
 fakul(N,Fak):- N>0,M is N-1,
              fakul(M,F1),
              Fak is N*F1.
 fakul(0,1).
 /* Die Bedingung N>0 darf in den obigen Klauseln nicht weggelassen werden! */
 
+% 3. Version, die einen cut nutzt, um unnoetiges Vorhalten von backtracking-Punkten
+% zu verhindern
 faku(0,1):- !.
 faku(N,Fak):- M is N-1,
               faku(M,F1),
