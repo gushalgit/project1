37febd95c08752c7aa27ecd23a7550adecb42635
mk55 <m55kern@icloud.com>
2014-08-05 17:21:10 +0200

    Zeitmessung mit LPA-Prolog gestrichen

--- test.pl
+++ test.pl
@@ -25,8 +25,3 @@ fakt(N,F):- fak_trans(N,1,F).
 fak_trans(N,Akk,F):- N>0, Akkneu is N*Akk, N1 is N-1,
                      fak_trans(N1,Akkneu,F).
 fak_trans(0,F,F).
-
-% ==================================================
-% Testpraedikate fuer LPA-Prolog
-tfak(N,F,Sec):- ms(fak(N,F),T), Sec is T/1200.
-tfakt(N,F,Sec):- ms(fakt(N,F),T), Sec is T/1200.
